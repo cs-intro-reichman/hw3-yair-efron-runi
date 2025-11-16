96f6592c2c6d2e856a93557c885256e04efaf239
Yair efron <yaire@MacBook-Air-sl-Yair.local>
2025-11-16 22:36:24 +0200

    first

--- Algebra.java
+++ Algebra.java
@@ -26,42 +26,66 @@ public class Algebra {
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 		// Replace the following statement with your code
-		return 0;
+		for(int i=1;i<=x2;i++)
+		{
+			x1++;
+		}
+		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for(int i=1;i<=x2;i++)
+		{
+			x1--;
+		}
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for(int i=1;i<=x2;i++)
+		{
+			x1=plus(x1,x1);
+		}
+		return x1;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		for(int i=1;i<=n;i++)
+		{
+			x=times(x, x);
+		}
+		return x;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int i=0;
+		for( i=1;x1!=0;i++)
+		{
+			x1=minus(x1,x2);
+		}
+		return i;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for(int i=1;i<=x2;i++)
+		{
+			x1=minus(x1,x2);
+		}
+		return x1;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		int i=0;
+		for( i=1;x!=pow(i, 2);i++)
+		{
+			
+		}
+		return i;
 	}	  	  
 }
