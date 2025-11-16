3b9211e78dbbe66c361af4077eecea6d481f1d64
Yair efron <yaire@MacBook-Air-sl-Yair.local>
2025-11-16 23:49:51 +0200

    second

--- Algebra.java
+++ Algebra.java
@@ -6,21 +6,21 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
+	    //System.out.println(plus(2,3));   // 2 + 3
+	    //System.out.println(minus(7,2));  // 7 - 2
+   		//System.out.println(minus(2,7));  // 2 - 7
+ 			//System.out.println(times(3,4));  // 3 * 4
+   		//System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
+   		//System.out.println(pow(5,3));      // 5^3
+   		//System.out.println(pow(3,5));      // 3^5
+   		//System.out.println(div(12,3));   // 12 / 3    
+   		//System.out.println(div(5,5));    // 5 / 5  
+   		//System.out.println(div(25,7));   // 25 / 7
+   		//System.out.println(mod(25,7));   // 25 % 7
+   		//System.out.println(mod(120,6));  // 120 % 6    
    		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
+			//System.out.println(sqrt(263169));
+   		//System.out.println(sqrt(76123));
 	}  
 
 	// Returns x1 + x2
@@ -30,6 +30,7 @@ public class Algebra {
 		{
 			x1++;
 		}
+		
 		return x1;
 	}
 
@@ -44,37 +45,39 @@ public class Algebra {
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		for(int i=1;i<=x2;i++)
+		int xreturn=x1;
+		for(int i=1;i<x2;i++)
 		{
-			x1=plus(x1,x1);
+			xreturn=plus(xreturn,x1);
 		}
-		return x1;
+		return xreturn;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		for(int i=1;i<=n;i++)
+		int start=x;
+		for(int i=1;i<n;i++)
 		{
-			x=times(x, x);
+			start=times(start, x);
+			
 		}
-		return x;
+		return start;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		int i=0;
-		for( i=1;x1!=0;i++)
-		{
-			x1=minus(x1,x2);
-		}
-		return i;
+		int count=0;
+		while (x1 >= x2) {
+    x1 = minus(x1, x2);
+    count++;
+	}
+		return count;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		for(int i=1;i<=x2;i++)
-		{
-			x1=minus(x1,x2);
+		while (x1 >= x2) {
+    x1 = minus(x1, x2);
 		}
 		return x1;
 	}	
@@ -84,7 +87,7 @@ public class Algebra {
 		int i=0;
 		for( i=1;x!=pow(i, 2);i++)
 		{
-			
+
 		}
 		return i;
 	}	  	  
